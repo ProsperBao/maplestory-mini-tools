dd08aff5217cae6b3ec8283ddf54a169c805e133
FuBaooo <baihe2233@gmail.com>
2023-12-26 23:05:57 +0800

    feat: add tool

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,56 +1,31 @@
 <script setup lang="ts">
-defineOptions({
-  name: 'IndexPage',
-})
-const user = useUserStore()
-const name = ref(user.savedName)
-
-const router = useRouter()
-function go() {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
-}
+defineOptions({ name: 'Guide' })
 
 const { t } = useI18n()
+
+interface GuideItem {
+  title: string
+  desc: string
+  path: string
+}
+
+const guideList: GuideItem[] = [
+  {
+    title: t('lachelein.guide.title'),
+    desc: t('lachelein.guide.desc'),
+    path: '/lachelein',
+  },
+]
 </script>
 
 <template>
   <div>
-    <div text-4xl>
-      <div i-carbon-campsite inline-block />
-    </div>
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-        Vitesse
-      </a>
-    </p>
-    <p>
-      <em text-sm opacity-75>{{ t('intro.desc') }}</em>
-    </p>
-
-    <div py-4 />
-
-    <TheInput
-      v-model="name"
-      :placeholder="t('intro.whats-your-name')"
-      autocomplete="false"
-      @keydown.enter="go"
-    />
-    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
-
-    <div>
-      <button
-        m-3 text-sm btn
-        :disabled="!name"
-        @click="go"
-      >
-        {{ t('button.go') }}
-      </button>
-    </div>
+    <ul>
+      <li v-for="item in guideList" :key="item.path">
+        <a :href="item.path" :title="item.desc">
+          {{ item.title }}
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
-
-<route lang="yaml">
-meta:
-  layout: home
-</route>
