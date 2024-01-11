3034c7565993683962260c38bb1a63c874c517cf
fubaooo <baihe2233@gmail.com>
2024-01-11 15:07:50 +0800

    fix: site title

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'MapleStory Widget',
   meta: [
     {
       name: 'description',
-      content: 'Opinionated Vite Starter Template',
+      content: 'a MapleStory widget',
     },
     {
       name: 'theme-color',
